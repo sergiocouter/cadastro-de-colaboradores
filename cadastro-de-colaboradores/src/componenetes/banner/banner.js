bc6b9297954579cd6b42f473da0fa0b182a58db0
sergiocouter <sergiocouter@gmail.com>
2023-04-20 16:58:56 -0300

    Criado banner do sistema

---
+++ cadastro-de-colaboradores/src/componenetes/banner/banner.js
@@ -0,0 +1,12 @@
+import './banner.css'
+
+function Banner() {
+    //JSX
+    return(
+        <reader className="banner">
+        <img src="/imagens/banner.png" alt="Banner do site"></img>
+        </reader>
+    )
+}
+
+export default Banner;
